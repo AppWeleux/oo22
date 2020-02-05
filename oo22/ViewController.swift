e4cafc5a5a2481f6a0e729906635131ae3f84951
AppWeleux Inc <developer@appweleux.com>
2020-02-05 13:14:36 +0000

    Initial Commit

---
+++ oo22/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oo22
+//
+//  Created by Mario Azevedo on 05/02/2020.
+//  Copyright © 2020 AppWeleuc Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
